6fa72a5660365438846cd45ffcc97fec652fde0b
Ruslan Musienko <106426703+gamarjkeee@users.noreply.github.com>
2023-11-26 09:49:33 +0200

    Add files via upload

--- selfpsy_care/js/app.js
+++ selfpsy_care/js/app.js
@@ -1,18 +1,5 @@
 (() => {
     "use strict";
-    function isWebp() {
-        function testWebP(callback) {
-            let webP = new Image;
-            webP.onload = webP.onerror = function() {
-                callback(webP.height == 2);
-            };
-            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-        }
-        testWebP((function(support) {
-            let className = support === true ? "webp" : "no-webp";
-            document.documentElement.classList.add(className);
-        }));
-    }
     let addWindowScrollEvent = false;
     setTimeout((() => {
         if (addWindowScrollEvent) {
@@ -23,5 +10,4 @@
         }
     }), 0);
     window["FLS"] = false;
-    isWebp();
 })();
