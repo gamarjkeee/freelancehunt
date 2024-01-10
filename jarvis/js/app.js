37277576fa4ce7d67736696bacc5ab9e542bb621
Ruslan Musienko <106426703+gamarjkeee@users.noreply.github.com>
2024-01-10 12:04:06 +0200

    Add files via upload

--- jarvis/js/app.js
+++ jarvis/js/app.js
@@ -10,7 +10,7 @@
             webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
         }
         testWebP((function(support) {
-            let className = support === true ? "no-webp" : "no-webp";
+            let className = support === true ? "webp" : "no-webp";
             document.documentElement.classList.add(className);
         }));
     }
