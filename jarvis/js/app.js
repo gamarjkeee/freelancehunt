aac81de04ed57279b801c66e2ff3d4cc558d798a
Ruslan Musienko <106426703+gamarjkeee@users.noreply.github.com>
2024-01-11 10:13:55 +0200

    Add files via upload

--- jarvis/js/app.js
+++ jarvis/js/app.js
@@ -14,6 +14,29 @@
             document.documentElement.classList.add(className);
         }));
     }
+    let isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+        }
+    };
+    function addTouchClass() {
+        if (isMobile.iOS()) document.documentElement.classList.add("no-anim");
+    }
     function addLoadedClass() {
         if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
             setTimeout((function() {
@@ -471,6 +494,7 @@
     da.init();
     window["FLS"] = false;
     isWebp();
+    addTouchClass();
     addLoadedClass();
     menuInit();
     pageNavigation();
